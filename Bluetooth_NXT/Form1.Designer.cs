190166dff31727a2d4e71593e6bd358e54a530cd
CCoDe4 <gory_stefanov@mail.bg>
2020-03-15 16:55:09 +0200

    First commit

---
+++ Bluetooth_NXT/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Bluetooth_NXT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.responseBox = new System.Windows.Forms.TextBox();
+            this.btnPing = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnLeftMotor = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // responseBox
+            // 
+            this.responseBox.Location = new System.Drawing.Point(88, 124);
+            this.responseBox.Name = "responseBox";
+            this.responseBox.Size = new System.Drawing.Size(100, 20);
+            this.responseBox.TabIndex = 0;
+            // 
+            // btnPing
+            // 
+            this.btnPing.Location = new System.Drawing.Point(235, 66);
+            this.btnPing.Name = "btnPing";
+            this.btnPing.Size = new System.Drawing.Size(75, 23);
+            this.btnPing.TabIndex = 1;
+            this.btnPing.Text = "Ping";
+            this.btnPing.UseVisualStyleBackColor = true;
+            this.btnPing.Click += new System.EventHandler(this.btnPing_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(453, 66);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(559, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Right";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnRightMotor_Click);
+            // 
+            // btnLeftMotor
+            // 
+            this.btnLeftMotor.Location = new System.Drawing.Point(416, 318);
+            this.btnLeftMotor.Name = "btnLeftMotor";
+            this.btnLeftMotor.Size = new System.Drawing.Size(72, 52);
+            this.btnLeftMotor.TabIndex = 4;
+            this.btnLeftMotor.Text = "Left";
+            this.btnLeftMotor.UseVisualStyleBackColor = true;
+            this.btnLeftMotor.Click += new System.EventHandler(this.btnLeftMotor_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Red;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStop.Location = new System.Drawing.Point(62, 220);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(206, 150);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "STOP MOTORS";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnLeftMotor);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnPing);
+            this.Controls.Add(this.responseBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox responseBox;
+        private System.Windows.Forms.Button btnPing;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLeftMotor;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Bluetooth_NXT/Form1.cs
@@ -0,0 +1,128 @@
+﻿using Lego.Mindstorms;
+using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Bluetooth_NXT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPing_Click(object sender, EventArgs e)
+        {
+            //  byte[] MessageLength = { 0x00, 0x00 };
+            byte[] MessageLength = { 0x06, 0x00 };
+            //  byte[] Command = { 0x01, 0x88 }; //get nxt version command  ----> response: 02 88 00 7C 01 1C 01 
+            byte[] Command = { 0x80, 0x03, 0x0B, 0x02, 0xF4, 0x01 }; //beep
+            //byte[] Command = { 0x01, 0x9B }; //get nxt version command ----> response: 02 9B 00 4E 58 54 00 00 00 00 00 00 00 00 00 00 00 00 00 16 53 0B 73 88 00 00 00 00 00 2C 55 01 00             
+
+            using (SerialPort BluetoothConnection = new SerialPort())
+            {
+                BluetoothConnection.PortName = "COM3";
+                BluetoothConnection.Open();
+                BluetoothConnection.ReadTimeout = 1500;
+
+                MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
+
+                BluetoothConnection.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
+                BluetoothConnection.Write(Command, 0, Command.Length); // send the message itself
+
+
+                // retrieve the reply length 
+                int length =
+                    BluetoothConnection.ReadByte() + 256 * BluetoothConnection.ReadByte();
+
+                // retrieve the reply data 
+                for (int i = 0; i < length; i++)
+                {
+                    responseBox.Text += BluetoothConnection.ReadByte().ToString("X2") + " ";
+                }
+
+                BluetoothConnection.Close();
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string portName = "COM3";
+
+            using (BluetoothCommunication bluetoothCommunication = new BluetoothCommunication(portName))
+            {
+                bluetoothCommunication.ConnectAsync();
+
+                Command command = new Command();
+                command.TurnMotorAtSpeed(OutputPort.A, 50);
+                command.TurnMotorAtSpeed(OutputPort.B, 50);
+
+            }
+
+        }
+
+        //NXT Program file extensions: *RBT*, RIC, RPG, *RXE*
+        //Null terminator 0x00
+        private void btnRightMotor_Click(object sender, EventArgs e)
+        {
+            byte[] MessageLength = { 0x0C, 0x00 };
+            //byte[] Command = { 0x6d, 0x6f, 0x74, 0x6f, 0x72, 0xC0, 0x80};
+            byte[] Command = { 0x80, 0x04, 0x02, 0x64, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
+            //6d6f746f72    727865
+
+            using (SerialPort BluetoothConnection = new SerialPort())
+            {
+                BluetoothConnection.PortName = "COM3";
+                BluetoothConnection.Open();
+                BluetoothConnection.ReadTimeout = 5000;
+
+                MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
+
+                BluetoothConnection.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
+                BluetoothConnection.Write(Command, 0, Command.Length); // send the message itself
+
+
+                // retrieve the reply length 
+                //if (Command[0] == 0x80)
+                //{
+                //    int length =
+                //   BluetoothConnection.ReadByte() + 256 * BluetoothConnection.ReadByte();
+
+                //    retrieve the reply data
+                //    for (int i = 0; i < length; i++)
+                //    {
+                //        responseBox.Text += BluetoothConnection.ReadByte().ToString("X2") + " ";
+                //    }
+                //}
+
+                BluetoothConnection.Close();
+            }
+        }
+
+        private void btnLeftMotor_Click(object sender, EventArgs e)
+        {
+            byte[] MessageLength = { 0x0C, 0x00 };
+            byte[] Command = { 0x80, 0x04, 0x01, 0x64, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
+
+            using (SerialPort BluetoothConnection = new SerialPort())
+            {
+                BluetoothConnection.PortName = "COM3";
+                BluetoothConnection.Open();
+                BluetoothConnection.ReadTimeout = 5000;
+
+                MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
+
+                BluetoothConnection.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
+                BluetoothConnection.Write(Command, 0, Command.Length); // send the message itself
+
+                BluetoothConnection.Close();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            C
+        }
+    }
+}

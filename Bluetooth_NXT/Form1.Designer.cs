e322613a2ca97e1d80190c0d770cbbdb4457e4a3
CCoDe4 <gory_stefanov@mail.bg>
2020-03-19 22:38:34 +0200

    Added controls by keyboard

--- Bluetooth_NXT/Form1.Designer.cs
+++ Bluetooth_NXT/Form1.Designer.cs
@@ -28,29 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.responseBox = new System.Windows.Forms.TextBox();
+            this.portName = new System.Windows.Forms.TextBox();
             this.btnPing = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.btnLeftMotor = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.btnUp = new System.Windows.Forms.Button();
             this.btnDown = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblPortName = new System.Windows.Forms.Label();
+            this.lblConnectionStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // responseBox
+            // portName
             // 
-            this.responseBox.Location = new System.Drawing.Point(182, 14);
-            this.responseBox.Name = "responseBox";
-            this.responseBox.Size = new System.Drawing.Size(100, 20);
-            this.responseBox.TabIndex = 0;
+            this.portName.Location = new System.Drawing.Point(76, 12);
+            this.portName.Name = "portName";
+            this.portName.Size = new System.Drawing.Size(70, 20);
+            this.portName.TabIndex = 1;
+            this.portName.Text = "COM3";
             // 
             // btnPing
             // 
-            this.btnPing.Location = new System.Drawing.Point(87, 12);
+            this.btnPing.Location = new System.Drawing.Point(309, 41);
             this.btnPing.Name = "btnPing";
-            this.btnPing.Size = new System.Drawing.Size(75, 23);
+            this.btnPing.Size = new System.Drawing.Size(97, 46);
             this.btnPing.TabIndex = 1;
-            this.btnPing.Text = "Ping";
+            this.btnPing.TabStop = false;
+            this.btnPing.Text = "Beep";
             this.btnPing.UseVisualStyleBackColor = true;
             this.btnPing.Click += new System.EventHandler(this.btnPing_Click);
             // 
@@ -60,6 +65,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(72, 52);
             this.button1.TabIndex = 3;
+            this.button1.TabStop = false;
             this.button1.Text = "Right";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.btnRightMotor_Click);
@@ -70,6 +76,7 @@
             this.btnLeftMotor.Name = "btnLeftMotor";
             this.btnLeftMotor.Size = new System.Drawing.Size(72, 52);
             this.btnLeftMotor.TabIndex = 4;
+            this.btnLeftMotor.TabStop = false;
             this.btnLeftMotor.Text = "Left";
             this.btnLeftMotor.UseVisualStyleBackColor = true;
             this.btnLeftMotor.Click += new System.EventHandler(this.btnLeftMotor_Click);
@@ -82,6 +89,7 @@
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(206, 150);
             this.btnStop.TabIndex = 5;
+            this.btnStop.TabStop = false;
             this.btnStop.Text = "STOP MOTORS";
             this.btnStop.UseVisualStyleBackColor = false;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
@@ -92,6 +100,7 @@
             this.btnUp.Name = "btnUp";
             this.btnUp.Size = new System.Drawing.Size(72, 52);
             this.btnUp.TabIndex = 6;
+            this.btnUp.TabStop = false;
             this.btnUp.Text = "Up";
             this.btnUp.UseVisualStyleBackColor = true;
             this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
@@ -102,24 +111,62 @@
             this.btnDown.Name = "btnDown";
             this.btnDown.Size = new System.Drawing.Size(72, 52);
             this.btnDown.TabIndex = 7;
+            this.btnDown.TabStop = false;
             this.btnDown.Text = "Down";
             this.btnDown.UseVisualStyleBackColor = true;
             this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(152, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 20);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.TabStop = false;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblPortName
+            // 
+            this.lblPortName.AutoSize = true;
+            this.lblPortName.Location = new System.Drawing.Point(14, 15);
+            this.lblPortName.Name = "lblPortName";
+            this.lblPortName.Size = new System.Drawing.Size(56, 13);
+            this.lblPortName.TabIndex = 9;
+            this.lblPortName.Text = "COM Port:";
+            // 
+            // lblConnectionStatus
+            // 
+            this.lblConnectionStatus.AutoSize = true;
+            this.lblConnectionStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblConnectionStatus.Location = new System.Drawing.Point(12, 35);
+            this.lblConnectionStatus.Name = "lblConnectionStatus";
+            this.lblConnectionStatus.Size = new System.Drawing.Size(73, 13);
+            this.lblConnectionStatus.TabIndex = 10;
+            this.lblConnectionStatus.Text = "Disconnected";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 261);
+            this.ClientSize = new System.Drawing.Size(654, 240);
+            this.Controls.Add(this.lblConnectionStatus);
+            this.Controls.Add(this.lblPortName);
+            this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.btnDown);
             this.Controls.Add(this.btnUp);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnLeftMotor);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnPing);
-            this.Controls.Add(this.responseBox);
+            this.Controls.Add(this.portName);
+            this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "RoboProject";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,13 +174,16 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox responseBox;
+        private System.Windows.Forms.TextBox portName;
         private System.Windows.Forms.Button btnPing;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnLeftMotor;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Button btnUp;
         private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblPortName;
+        private System.Windows.Forms.Label lblConnectionStatus;
     }
 }
 
--- Bluetooth_NXT/Form1.cs
+++ Bluetooth_NXT/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Bluetooth_NXT.Common;
 using System;
+using System.Drawing;
 using System.IO.Ports;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,22 +13,16 @@ namespace Bluetooth_NXT
         public Form1()
         {
             InitializeComponent();
-            motorManager = new MotorManager();
 
-            PrepareBluetoothConnection();
-        }
+            portName.Text = "COM3";
 
-        private void PrepareBluetoothConnection()
-        {
+            roboManager = new RoboManager();
             bluetooth = new SerialPort();
-            bluetooth.PortName = "COM3";
-
-            bluetooth.Open();
-            bluetooth.ReadTimeout = 1500;
-
+            this.ActiveControl = null;
+            
         }
 
-        public MotorManager motorManager { get; set; }
+        public RoboManager roboManager { get; set; }
         private SerialPort bluetooth { get; set; }
 
         /// <summary>
@@ -39,39 +34,8 @@ namespace Bluetooth_NXT
         {
             await Task.Run(() =>
             {
-               
-                    try
-                    {
-                        //  byte[] MessageLength = { 0x00, 0x00 };
-                        byte[] MessageLength = { 0x06, 0x00 };
-                        //  byte[] Command = { 0x01, 0x88 }; //get nxt version command  ----> response: 02 88 00 7C 01 1C 01 
-                        byte[] Command = { 0x80, 0x03, 0x0B, 0x02, 0xF4, 0x01 }; //beep
-
-                        MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
-
-                        bluetooth.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
-                        bluetooth.Write(Command, 0, Command.Length); // send the message itself
-
-
-                        // retrieve the reply length 
-                        //int length =
-                        //    BluetoothConnection.ReadByte() + 256 * BluetoothConnection.ReadByte();
-
-                        //// retrieve the reply data 
-                        //for (int i = 0; i < length; i++)
-                        //{
-                        //    responseBox.Text += BluetoothConnection.ReadByte().ToString("X2") + " ";
-                        //}
-
-                    }
-                    catch (Exception ex)
-                    {
-
-                        return;
-                    }
-
+                roboManager.Beep(bluetooth);
             });
-
         }
 
         //NXT Program file extensions: *RBT*, RIC, RPG, *RXE*
@@ -80,36 +44,7 @@ namespace Bluetooth_NXT
         {
             await Task.Run(() =>
             {
-                try
-                {
-                    byte[] MessageLength = { 0x0C, 0x00 };
-                    //byte[] Command = { 0x6d, 0x6f, 0x74, 0x6f, 0x72, 0xC0, 0x80};
-                    byte[] Command = { 0x80, 0x04, 0x00, 0x32, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
-
-                    MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
-
-                    bluetooth.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
-                    bluetooth.Write(Command, 0, Command.Length); // send the message itself
-
-
-                    // retrieve the reply length 
-                    //if (Command[0] == 0x80)
-                    //{
-                    //    int length =
-                    //   BluetoothConnection.ReadByte() + 256 * BluetoothConnection.ReadByte();
-
-                    //    retrieve the reply data
-                    //    for (int i = 0; i < length; i++)
-                    //    {
-                    //        responseBox.Text += BluetoothConnection.ReadByte().ToString("X2") + " ";
-                    //    }
-                    //}
-
-                }
-                catch (Exception ex)
-                {
-                    return;
-                }
+                roboManager.LeftMotor(bluetooth); //In order to turn right, left motor is activated.
             });
 
         }
@@ -118,25 +53,7 @@ namespace Bluetooth_NXT
         {
             await Task.Run(() =>
             {
-
-                try
-                {
-                    byte[] MessageLength = { 0x0C, 0x00 };
-                    byte[] Command = { 0x80, 0x04, 0x01, 0x32, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
-
-
-                    MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
-
-                    bluetooth.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
-                    bluetooth.Write(Command, 0, Command.Length); // send the message itself
-
-                }
-                catch (Exception ex)
-                {
-
-                    return;
-                }
-
+                roboManager.RightMotor(bluetooth); //In order to turn left, right motor is activated.
             });
         }
 
@@ -144,107 +61,76 @@ namespace Bluetooth_NXT
         {
             await Task.Run(() =>
             {
-                try
-                {
-                    byte[] MessageLength = { 0x0C, 0x00 };
-                    byte[] Command = { 0x80, 0x04, 0xFF, 0x00, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
-
-                    MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
-
-                    bluetooth.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
-                    bluetooth.Write(Command, 0, Command.Length); // send the message itself
-
-                }
-                catch (Exception ex)
-                {
-
-                    return;
-                }
+                roboManager.StopMotors(bluetooth);
             });
-
         }
 
         private async void btnUp_Click(object sender, EventArgs e)
         {
             await Task.Run(() =>
             {
-
-                try
-                {
-                    byte[] MessageLength = { 0x0C, 0x00 };
-                    //byte[] Command = { 0x6d, 0x6f, 0x74, 0x6f, 0x72, 0xC0, 0x80};
-                    byte[] Command = { 0x80, 0x04, 0xFF, 0x32, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
-
-
-                    MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
-
-                    bluetooth.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
-                    bluetooth.Write(Command, 0, Command.Length); // send the message itself
-
-
-                    // retrieve the reply length 
-                    //if (Command[0] == 0x80)
-                    //{
-                    //    int length =
-                    //   BluetoothConnection.ReadByte() + 256 * BluetoothConnection.ReadByte();
-
-                    //    retrieve the reply data
-                    //    for (int i = 0; i < length; i++)
-                    //    {
-                    //        responseBox.Text += BluetoothConnection.ReadByte().ToString("X2") + " ";
-                    //    }
-                    //}
-
-                }
-                catch (Exception ex)
-                {
-                    return;
-                }
-
+                roboManager.MoveForwards(bluetooth);
             });
         }
 
         private async void btnDown_Click(object sender, EventArgs e)
         {
-
             await Task.Run(() =>
             {
-
-              
-                    try
-                    {
-                        byte[] MessageLength = { 0x0C, 0x00 };
-                        //byte[] Command = { 0x6d, 0x6f, 0x74, 0x6f, 0x72, 0xC0, 0x80};
-                        byte[] Command = { 0x80, 0x04, 0xFF, 0xCE, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
-
-                        MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
-
-                        bluetooth.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
-                        bluetooth.Write(Command, 0, Command.Length); // send the message itself
-
-                        // retrieve the reply length 
-                        //if (Command[0] == 0x80)
-                        //{
-                        //    int length =
-                        //   BluetoothConnection.ReadByte() + 256 * BluetoothConnection.ReadByte();
-
-                        //    retrieve the reply data
-                        //    for (int i = 0; i < length; i++)
-                        //    {
-                        //        responseBox.Text += BluetoothConnection.ReadByte().ToString("X2") + " ";
-                        //    }
-                        //}
-
-                    }
-                    catch (Exception ex)
-                    {
-                        return;
-                    }
-
+                roboManager.MoveBackwards(bluetooth);
             });
-
         }
 
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            bluetooth = roboManager.Connect(bluetooth, portName.Text);
 
+            if (bluetooth.IsOpen)
+            {
+                lblConnectionStatus.Text = "Connected";
+                lblConnectionStatus.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblConnectionStatus.Text = "Disconnected";
+                lblConnectionStatus.ForeColor = Color.Red;
+
+                MessageBox.Show("Check if robot is ON or the bluetooth device", "ERROR");
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!bluetooth.IsOpen) return;
+
+            roboManager.StopMotors(bluetooth);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+         {
+            if (!bluetooth.IsOpen) return; 
+
+            switch (e.KeyData)
+            {
+                case Keys.Up:
+                    roboManager.MoveForwards(bluetooth);
+                    break;
+                case Keys.Down:
+                    roboManager.MoveBackwards(bluetooth);
+                    break;
+                case Keys.Left:
+                    roboManager.RightMotor(bluetooth);
+                    break;
+                case Keys.Right:
+                    roboManager.LeftMotor(bluetooth);
+                    break;
+                case Keys.Space:
+                    roboManager.StopMotors(bluetooth);
+                    break;
+                case Keys.Back:
+                    roboManager.Beep(bluetooth);
+                    break;
+            }
+        }
     }
 }

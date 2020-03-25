1ae546660bb898121055fb6fa14acc09d88415ee
Goran <goran_stefanov@abv.bg>
2020-03-26 01:23:44 +0200

    Established connection to the remote device

--- MobileHMI/MobileHMI.Android/BroadcastReceivers/BluetoothDeviceDetector.cs
+++ MobileHMI/MobileHMI.Android/BroadcastReceivers/BluetoothDeviceDetector.cs
@@ -10,6 +10,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.Forms;
 
 namespace MobileHMI.Droid.BroadcastReceivers
 {
@@ -30,6 +31,7 @@ namespace MobileHMI.Droid.BroadcastReceivers
                 if (newDevice.Name == "NXT")
                 {
                     Toast.MakeText(context, "NXT is found.", ToastLength.Short).Show();
+                    MessagingCenter.Send<BluetoothDevice>(newDevice, "NXT_Robot");
                 }
             }
         }
--- MobileHMI/MobileHMI/MainPage.xaml.cs
+++ MobileHMI/MobileHMI/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Android.Bluetooth;
+using Java.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,12 @@ namespace MobileHMI
     {
         public MainPage()
         {
-            InitializeComponent();            
+            InitializeComponent();
+
+            MessagingCenter.Subscribe<BluetoothDevice>(this, "NXT_Robot", (sender) => 
+            {
+                ConnectToDevice(sender);
+            });
         }
 
         public void StartScan()
@@ -25,9 +31,28 @@ namespace MobileHMI
             }
         }
 
-        private void Connect(object sender, EventArgs e)
+        private void ConnectToDevice(BluetoothDevice device)
         {
+            try
+            {
+                var _socket = device.CreateRfcommSocketToServiceRecord(UUID.FromString("00001101-0000-1000-8000-00805f9b34fb"));//Note that the UUID specified below is the standard UUID for SPP
+                _socket.Connect();
 
+                byte[] MessageLength = { 0x0C, 0x00 };
+                //byte[] Command = { 0x6d, 0x6f, 0x74, 0x6f, 0x72, 0xC0, 0x80};
+                byte[] Command = { 0x80, 0x04, 0xFF, 0x64, 0x07, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00 }; //test
+
+                MessageLength[0] = (byte)Command.Length; //set the LSB(least significant bit) to the length of the message 
+
+                _socket.OutputStream.Write(MessageLength, 0, MessageLength.Length); //send the 2 bytes header 
+                _socket.OutputStream.Write(Command, 0, Command.Length); // send the message itself      
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+                  
         }
 
         private async void ScanForDevices(object sender, EventArgs e)
